77a3c8de3e4a809ced353d3f103cdca234b65242
Marcela Mazo <marcela.mazo@correounivalle.edu.co>
2024-04-03 20:36:18 -0500

    Agregadas luces puntuales a cada RedMenTriangle

--- src/world/red-mens/RedManTriangle.jsx
+++ src/world/red-mens/RedManTriangle.jsx
@@ -1,11 +1,15 @@
 import { useGLTF } from "@react-three/drei"
 import { useFrame } from "@react-three/fiber"
 import { useRef } from "react"
+import { PointLight } from "three"; // Importamos PointLight de Three.js
 
 export default function RedManTriangle(props) {
   const redManTriangleRef = useRef(null)
   const { nodes, materials } = useGLTF('/assets/models/red-mens/RedManTriangle.glb')
   const speed = 4
+
+  // Agregamos una referencia a la luz puntual
+  const pointLightRef = useRef(new PointLight(0xff0000, 1, 100));
 
   useFrame((state, delta) => {
     const position = redManTriangleRef.current.position
@@ -25,10 +29,14 @@ export default function RedManTriangle(props) {
       position.y = 0.0;
     }
 
+    // Actualizamos la posición de la luz puntual para que siga a cada instancia de RedManTriangle
+    pointLightRef.current.position.copy(position);
   })
 
   return (
     <group {...props} ref={redManTriangleRef} dispose={null}>
+      {/* Añadimos la luz puntual al grupo del RedManTriangle */}
+      <pointLight ref={pointLightRef} color={0xff0000} intensity={1} distance={100} />
       <group name="Scene">
         <group name="Rigid">
           <skinnedMesh
