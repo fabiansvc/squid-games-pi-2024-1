8101fb7a8a3c22139b413859aacf270e287ce512
Fabian Valencia <fabian.cordoba@correounivalle.edu.co>
2024-04-13 17:01:41 -0500

    Sesion 7 implemented

--- src/pages/level1/characters/avatar/Avatar.jsx
+++ src/pages/level1/characters/avatar/Avatar.jsx
@@ -1,25 +1,110 @@
-import { RigidBody } from "@react-three/rapier";
+import { CapsuleCollider, CuboidCollider, RigidBody } from "@react-three/rapier";
 import { useEffect, useRef } from "react";
 import { useAvatar } from "../../../../context/AvatarContext";
+import { useAnimations, useGLTF } from "@react-three/drei";
 
 export default function Avatar() {
     const avatarBodyRef = useRef();
     const avatarRef = useRef();
-    const {setAvatar} = useAvatar();
+    const { avatar, setAvatar } = useAvatar();
+    const { nodes, materials, animations } = useGLTF('/assets/models/avatar/Avatar.glb')
 
-    useEffect(()=>{
-        setAvatar({
-            ref: avatarRef.current,
-            body: avatarBodyRef.current
-        })
-    }, [avatarBodyRef.current, avatarRef.current])
-    
+    const { actions } = useAnimations(animations, avatarRef)
+
+    // useEffect(() => {
+    //     setAvatar({
+    //         ref: avatarRef.current,
+    //         body: avatarBodyRef.current
+    //     })
+    // }, [avatarBodyRef.current, avatarRef.current])
+
+    useEffect(() => {
+        actions[avatar.animation]?.reset().fadeIn(0.5).play();
+        return () => {
+            if (actions[avatar.animation])
+                actions[avatar.animation].fadeOut(0.5);
+        }
+
+    }, [actions, avatar.animation]);
+
     return (
-        <RigidBody ref={avatarBodyRef} position={[0, 1, -3]} colliders="ball">
-            <mesh ref={avatarRef}>
-                <sphereGeometry args={[0.2, 32, 32]} />
-                <meshStandardMaterial color="red" />
-            </mesh>
-        </RigidBody>
+        // <RigidBody ref={avatarBodyRef} position={[0, 1, -3]} colliders={false}>
+            <group ref={avatarRef} name="Scene" position-y={-0.65}>
+                <group name="Armature">
+                    <skinnedMesh
+                        name="EyeLeft"
+                        geometry={nodes.EyeLeft.geometry}
+                        material={materials.Wolf3D_Eye}
+                        skeleton={nodes.EyeLeft.skeleton}
+                        morphTargetDictionary={nodes.EyeLeft.morphTargetDictionary}
+                        morphTargetInfluences={nodes.EyeLeft.morphTargetInfluences}
+                    />
+                    <skinnedMesh
+                        name="EyeRight"
+                        geometry={nodes.EyeRight.geometry}
+                        material={materials.Wolf3D_Eye}
+                        skeleton={nodes.EyeRight.skeleton}
+                        morphTargetDictionary={nodes.EyeRight.morphTargetDictionary}
+                        morphTargetInfluences={nodes.EyeRight.morphTargetInfluences}
+                    />
+                    <skinnedMesh
+                        name="Wolf3D_Body"
+                        geometry={nodes.Wolf3D_Body.geometry}
+                        material={materials.Wolf3D_Body}
+                        skeleton={nodes.Wolf3D_Body.skeleton}
+                        morphTargetDictionary={nodes.Wolf3D_Body.morphTargetDictionary}
+                        morphTargetInfluences={nodes.Wolf3D_Body.morphTargetInfluences}
+                    />
+                    <skinnedMesh
+                        name="Wolf3D_Hair"
+                        geometry={nodes.Wolf3D_Hair.geometry}
+                        material={materials.Wolf3D_Hair}
+                        skeleton={nodes.Wolf3D_Hair.skeleton}
+                    />
+                    <skinnedMesh
+                        name="Wolf3D_Head"
+                        geometry={nodes.Wolf3D_Head.geometry}
+                        material={materials.Wolf3D_Skin}
+                        skeleton={nodes.Wolf3D_Head.skeleton}
+                        morphTargetDictionary={nodes.Wolf3D_Head.morphTargetDictionary}
+                        morphTargetInfluences={nodes.Wolf3D_Head.morphTargetInfluences}
+                    />
+                    <skinnedMesh
+                        name="Wolf3D_Outfit_Bottom"
+                        geometry={nodes.Wolf3D_Outfit_Bottom.geometry}
+                        material={materials.Wolf3D_Outfit_Bottom}
+                        skeleton={nodes.Wolf3D_Outfit_Bottom.skeleton}
+                        morphTargetDictionary={nodes.Wolf3D_Outfit_Bottom.morphTargetDictionary}
+                        morphTargetInfluences={nodes.Wolf3D_Outfit_Bottom.morphTargetInfluences}
+                    />
+                    <skinnedMesh
+                        name="Wolf3D_Outfit_Footwear"
+                        geometry={nodes.Wolf3D_Outfit_Footwear.geometry}
+                        material={materials.Wolf3D_Outfit_Footwear}
+                        skeleton={nodes.Wolf3D_Outfit_Footwear.skeleton}
+                        morphTargetDictionary={nodes.Wolf3D_Outfit_Footwear.morphTargetDictionary}
+                        morphTargetInfluences={nodes.Wolf3D_Outfit_Footwear.morphTargetInfluences}
+                    />
+                    <skinnedMesh
+                        name="Wolf3D_Outfit_Top"
+                        geometry={nodes.Wolf3D_Outfit_Top.geometry}
+                        material={materials.Wolf3D_Outfit_Top}
+                        skeleton={nodes.Wolf3D_Outfit_Top.skeleton}
+                        morphTargetDictionary={nodes.Wolf3D_Outfit_Top.morphTargetDictionary}
+                        morphTargetInfluences={nodes.Wolf3D_Outfit_Top.morphTargetInfluences}
+                    />
+                    <skinnedMesh
+                        name="Wolf3D_Teeth"
+                        geometry={nodes.Wolf3D_Teeth.geometry}
+                        material={materials.Wolf3D_Teeth}
+                        skeleton={nodes.Wolf3D_Teeth.skeleton}
+                        morphTargetDictionary={nodes.Wolf3D_Teeth.morphTargetDictionary}
+                        morphTargetInfluences={nodes.Wolf3D_Teeth.morphTargetInfluences}
+                    />
+                    <primitive object={nodes.Hips} />
+                    {/* <CuboidCollider args={[0.2, 0.75, 0.2]} position={[0, 0.75, 0]} /> */}
+                </group>
+            </group>
+        // </RigidBody>
     )
 }
