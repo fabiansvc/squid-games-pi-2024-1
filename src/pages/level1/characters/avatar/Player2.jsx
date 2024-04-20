06c9a63613dbce76791a593af2a64ab19bebf8df
Fabian Valencia <fabian.cordoba@correounivalle.edu.co>
2024-04-20 16:43:25 -0500

    Sesion 8 implemented with websockets and issue fixed

--- src/pages/level1/characters/avatar/Player2.jsx
+++ src/pages/level1/characters/avatar/Player2.jsx
@@ -7,8 +7,6 @@ import { socket } from '../../../../socket/socket-manager'
 export function Player2({ url }) {
     const player2Ref = useRef()
     const rigidBodyPlayer2Ref = useRef()
-    const { nodes, materials } = useGLTF(url)
-    const [animation, setAnimation] = useState('Idle')
 
     const position = vec3();
 
@@ -16,19 +14,7 @@ export function Player2({ url }) {
         position.set(player.position.x, player.position.y, player.position.z)
         rigidBodyPlayer2Ref.current?.setTranslation(position, true)
     })
-
-    socket.on('update-animation', (animation) => {
-        setAnimation(animation)
-    })
-
-    useEffect(() => {
-        actions[animation]?.reset().fadeIn(0.5).play();
-        return () => {
-            if (actions[animation])
-                actions[animation].fadeOut(0.5);
-        }
-    }, [actions, animation]);
-
+    
     return (
         <RigidBody ref={rigidBodyPlayer2Ref} colliders={false} type='fixed' position={[-2, 4, 0]}>
             <group ref={player2Ref} dispose={null}>
